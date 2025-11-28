18ae2af9d6f57195ed483e9ba81598ec7ac10584
Prasad2299 <prasadramphale354@gmail.com>
2025-11-29 00:01:58 +0530

    exploring get n post

--- src/app.js
+++ src/app.js
@@ -2,23 +2,12 @@ const express = require("express")
 
 const app = express()
 
-//order of routing is important
-
-app.use("/hello/2",(req,res)=>{
-  res.send("hello from hello 2")
+app.get("/user",(req,res)=>{
+  res.send({name:"prasad"})
 })
 
-app.use("/hello",(req,res)=>{
-  res.send("hello from hello request")
-})
-
-// app.use("/hello/2",(req,res)=>{
-//   res.send("hello from hello 2")
-// })
-
-//function inside app.use is request handler function respond to incoming request on server.
-app.use((req,res)=>{
-  res.send("Hello world from server!")
+app.post("/user",(req,res)=>{
+  res.send("data saved to db successfully!")
 })
 
 app.listen(3000,()=>{
