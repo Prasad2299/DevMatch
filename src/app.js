07a6dcf65dcbd900f5ebcda7d40360f2039465cf
Prasad2299 <prasadramphale354@gmail.com>
2025-12-08 20:06:55 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,27 +1,20 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.get("/user",(req,res)=>{
-  console.log(req.query)// to acces data from req.query parameter
-  res.send({name:"prasad"})
-})
-
-app.get("/user/:userId",(req,res)=>{
-  console.log(req.params)// to acces data from dynamic values from request parameter
-  res.send({name:"prasad"})
-})
-
-app.get("/user/:userId/:name",(req,res)=>{
-  console.log(req.params)// to acces data from dynamic values from request parameter
-  res.send({name:"prasad"})
-})
+app.use(
+  "/users",
+  (req, res) => {
+    console.log("Response 1");
+    res.send("Response 1");
+  },
+  (req, res) => {
+    console.log("Response 2");
+    res.send("Response 2");
+  }
+);
 
 
-app.post("/user",(req,res)=>{
-  res.send("data saved to db successfully!")
-})
-
-app.listen(3000,()=>{
-  console.log("Server is running on port 3000")
-})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
