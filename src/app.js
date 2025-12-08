a456747d0edcc3ca4300629ebf54d6349b3c7888
Prasad2299 <prasadramphale354@gmail.com>
2025-12-08 23:59:03 +0530

    authorization middleware

--- src/app.js
+++ src/app.js
@@ -2,28 +2,28 @@ const express = require("express");
 
 const app = express();
 
-app.use(
-  "/users",
-  [(req, res,next) => {
-    console.log("Response 1");
-    // res.send("Response 1");
-    next()
-  },
-  (req, res,next) => {
-    console.log("Response 2");
-    res.send("Response 2");
-    next()
-  },
-  (req, res,next) => {
-    console.log("Response 3");
-    res.send("Response 3");
-    next()
-  },
-  (req, res) => {
-    console.log("Response 4");
-    res.send("Response 4");
-  }]
-);
+
+app.get("/admin/getAllData",(req,res)=>{
+  console.log("get All data")
+  const token = "xyz"
+  const isAdminAutherized = token === "xyz"
+  if(isAdminAutherized){
+    res.send("data has been fetched!")
+  }else{
+    res.status(401).send("Unauthorized Admin!!!")
+  }
+})
+
+app.delete("/admin/deleteData",(req,res)=>{
+  console.log("get All data")
+  const token = "xyz"
+  const isAdminAutherized = token === "xyz"
+  if(isAdminAutherized){
+    res.send("data has been deletedS!")
+  }else{
+    res.status(401).send("Unauthorized Admin!!!")
+  }
+})
 
 
 app.listen(3000, () => {
