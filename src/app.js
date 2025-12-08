84a44a50d6dddcc7418ae0ae8f3ee9b4cfc17e38
Prasad2299 <prasadramphale354@gmail.com>
2025-12-09 00:33:39 +0530

    error handling using app.use()

--- src/app.js
+++ src/app.js
@@ -2,34 +2,24 @@ const express = require("express");
 
 const app = express();
 
-const {authAdmin,authUser} =  require('./middlewares/auth')
 
-// app.use("/admin",(req,res,next)=>{
-//   const token = "xyz"
-//   const isAdminAutherized = token === "xyz"
-//   if(!isAdminAutherized){
-//     res.status(401).send("Unauthorized Admin!!!")
-//   }else{
-//     next()
-//   }
-// })
 
-app.post("/user/login",(req,res)=>{
-  res.send("User login!")
-})
 
-app.get("/user/getUserData",authUser,(req,res)=>{
+app.get("/user/getUserData",(req,res)=>{
+  //logic for database and fetched data
+
+  // if there is error while fetching data
+
+  throw new Error("sdfefdfds")
+
   res.send("user data fetched!")
 })
 
-app.get("/admin/getAllData",authAdmin,(req,res)=>{
-  console.log("get All data")
-  res.send("data fetched!!")
-})
-
-app.delete("/admin/deleteData",authAdmin,(req,res)=>{
-  console.log("get All data")
-  res.send("data deleted!!")
+//always put this error middleware at last 
+app.use("/",(err,req,res,next)=>{
+  if(err){
+    res.status(500).send("Something went wrong!")
+  }
 })
 
 
