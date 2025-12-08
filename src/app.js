2fc25944d7c2c8aac783f0c1e8c6c9a9b32c9c03
Prasad2299 <prasadramphale354@gmail.com>
2025-12-08 20:10:45 +0530

    mutiple route handler with array

--- src/app.js
+++ src/app.js
@@ -4,15 +4,25 @@ const app = express();
 
 app.use(
   "/users",
-  (req, res,next) => {
+  [(req, res,next) => {
     console.log("Response 1");
-    res.send("Response 1");
+    // res.send("Response 1");
+    next()
+  },
+  (req, res,next) => {
+    console.log("Response 2");
+    res.send("Response 2");
+    next()
+  },
+  (req, res,next) => {
+    console.log("Response 3");
+    res.send("Response 3");
     next()
   },
   (req, res) => {
-    console.log("Response 2");
-    res.send("Response 2");
-  }
+    console.log("Response 4");
+    res.send("Response 4");
+  }]
 );
 
 
