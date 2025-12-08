07334cc064b123886ef0d750f8bd07798ab2ef44
Prasad2299 <prasadramphale354@gmail.com>
2025-12-09 00:09:28 +0530

    auth middleware for user and admin

--- src/app.js
+++ src/app.js
@@ -2,22 +2,32 @@ const express = require("express");
 
 const app = express();
 
-app.use("/admin",(req,res,next)=>{
-  const token = "xyz"
-  const isAdminAutherized = token === "xyz"
-  if(!isAdminAutherized){
-    res.status(401).send("Unauthorized Admin!!!")
-  }else{
-    next()
-  }
+const {authAdmin,authUser} =  require('./middlewares/auth')
+
+// app.use("/admin",(req,res,next)=>{
+//   const token = "xyz"
+//   const isAdminAutherized = token === "xyz"
+//   if(!isAdminAutherized){
+//     res.status(401).send("Unauthorized Admin!!!")
+//   }else{
+//     next()
+//   }
+// })
+
+app.post("/user/login",(req,res)=>{
+  res.send("User login!")
 })
 
-app.get("/admin/getAllData",(req,res)=>{
+app.get("/user/getUserData",authUser,(req,res)=>{
+  res.send("user data fetched!")
+})
+
+app.get("/admin/getAllData",authAdmin,(req,res)=>{
   console.log("get All data")
-  res.send("data fetched!")
+  res.send("data fetched!!")
 })
 
-app.delete("/admin/deleteData",(req,res)=>{
+app.delete("/admin/deleteData",authAdmin,(req,res)=>{
   console.log("get All data")
   res.send("data deleted!!")
 })
