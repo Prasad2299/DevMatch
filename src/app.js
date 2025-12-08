5b0270c45ecc95092cadd3a912f518d04e2149d0
Prasad2299 <prasadramphale354@gmail.com>
2025-12-09 00:35:44 +0530

    error handling using try catch most use way

--- src/app.js
+++ src/app.js
@@ -7,20 +7,24 @@ const app = express();
 
 app.get("/user/getUserData",(req,res)=>{
   //logic for database and fetched data
-
+  try {
   // if there is error while fetching data
 
   throw new Error("sdfefdfds")
 
-  res.send("user data fetched!")
+  res.send("user data fetched!")    
+  } catch (error) {
+    console.log(error)
+    res.status(500).send("Something went wrong!!")
+  }
 })
 
 //always put this error middleware at last 
-app.use("/",(err,req,res,next)=>{
-  if(err){
-    res.status(500).send("Something went wrong!")
-  }
-})
+// app.use("/",(err,req,res,next)=>{
+//   if(err){
+//     res.status(500).send("Something went wrong!")
+//   }
+// })
 
 
 app.listen(3000, () => {
