116e8174cab1b4a902e8bf901205d817314e265d
Prasad2299 <prasadramphale354@gmail.com>
2026-01-19 18:17:41 +0530

    dynamic data to api

--- src/app.js
+++ src/app.js
@@ -4,9 +4,10 @@ const { User } = require("./models/user");
 
 const app = express();
 
+app.use(express.json())
 
 app.post("/signup",async(req,res)=>{
-
+  console.log(req.body)
   const userObj = {
     firstName:"Prasad",
     lastName:"Ramphale",
--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
 const connectDb = async () => {
+  console.log("inside")
   await mongoose.connect(
-    "mongodb+srv://developer:ykVxBevEyRSyJG9I@nodejs.zpwmuzl.mongodb.net/devMatch"// connecting to devMatch database from mongodb cluster
+    "mongodb+srv://developer:developer@nodejs.zpwmuzl.mongodb.net/devMatch?appName=nodejs"
+    // "mongodb+srv://developer:ykVxBevEyRSyJG9I@nodejs.zpwmuzl.mongodb.net/devMatch"// connecting to devMatch database from mongodb cluster
   );
 };
 
