38de13ca58b74a8607458607e61aa9351afda058
Prasad2299 <prasadramphale354@gmail.com>
2026-02-26 16:19:27 +0530

    addded feed api with pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { authUser } = require('../middlewares/auth');
 const ConnectionRequest = require('../models/connectionRequest');
+const { User } = require('../models/user');
 
 const userRouter = express.Router()
 
@@ -70,6 +71,52 @@ userRouter.get("/user/connections",authUser,async(req,res)=>{
       message:'Something went wrong!'
     })
   }
+})
+
+// get user feed
+
+userRouter.get("/user/feed",authUser,async(req,res)=>{
+  try{
+    const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    //find out loggedin user connection
+    const connRequest = await ConnectionRequest.find({
+      $or:[{fromUserId:loggedInUser._id},{toUserId:loggedInUser._id}]
+    }).select("fromUserId toUserId")
+    console.log(connRequest)
+    
+    const hideUserFromFeed = new Set()
+
+    //add unique data
+    connRequest.forEach((req)=>{
+      hideUserFromFeed.add(req.fromUserId.toString())
+      hideUserFromFeed.add(req.toUserId.toString())
+    })
+    console.log(hideUserFromFeed)
+
+    const users = await User.find({
+      $and:[
+        {_id:{$nin:Array.from(hideUserFromFeed)}},
+        {_id:{$ne:loggedInUser._id}}
+      ]
+    }).select("firstName lastName age gender photoURL about skills").skip(skip).limit(limit)
+
+    return res.status(200).json({
+      message: `${loggedInUser.firstName} , it's your feed!`,
+      users
+    })
+
+  }catch(error){
+    console.log(error)
+    return res.status(400).json({
+      message:'Something went wrong!'
+    })
+  }
+
 })
 
 module.exports = userRouter
